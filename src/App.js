394c9e25192594bb45f44bb435a77540bd4fa109
smazad1212 <smazad1212@gmail.com>
2017-11-14 05:57:45 +0600

    react rounter initiallized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 import Home from './components/Home';
+import Counter from './components/Counter';
 
 import './App.css';
 
 const App = () => (
-  <Home/>
+  <Router>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/counter" component={Counter}/>
+      <Route path="/topics" component={Home}/>
+    </div>
+  </Router>
 )
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import {
+  Link
+} from 'react-router-dom';
 import Counter from './Counter';
 
 const Home = () => {
@@ -8,10 +10,9 @@ const Home = () => {
     <header>
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/counter">Counter</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/counter">Counter</Link></li>
         </ul>
       </nav>
     </header>
